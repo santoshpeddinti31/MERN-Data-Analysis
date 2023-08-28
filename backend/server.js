bb296c2d0a1cc908bfc44b21fd8328b4e94e1fbd
santosh peddinti <santoshpeddinti1997@gmail.com>
2023-08-28 11:26:54 +0530

    'worked'

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://mern-data-backend-development.vercel.app/",
     methods: ["GET"],
     credentials: true,
   })
